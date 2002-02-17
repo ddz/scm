91e8bf6f27ca1b89ae3715f691ee92f5a521381b
Dino A. Dai Zovi <3083278+ddz@users.noreply.github.com>
2002-02-17 04:15:20 +0000

    Added symbol table test suite.

---
+++ test/test-symbols.c
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include "scheme.h"
+
+char* symchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!$%&*/:<=>?^_~0123456789+-.@";
+
+int main(int argc, char* argv[])
+{
+    int i, n = strlen(symchars);
+    char buf[1024];
+
+    srand(0);
+    
+    for (i = 0; i < 1000; i++) {
+        int j, s = rand() % 1024;
+        for (j = 0; j < s; j++)
+            buf[j] = symchars[rand() % n];
+        buf[j] = '\0';
+
+        make_symbol(buf, s);
+    }
+}
