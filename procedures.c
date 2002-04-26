294ef35da3f18d4bb21d3aa5d7f85732b6f61a3f
Dino A. Dai Zovi <3083278+ddz@users.noreply.github.com>
2002-04-26 05:30:12 +0000

    Working on rehauling primative procedures.

--- procedures.c
+++ procedures.c
@@ -6,20 +6,6 @@
 #include <stdlib.h>
 #include "scheme.h"
 
-scheme_t make_primative(scheme_t (*f)(scheme_t))
-{
-    scheme_t s = MAKE_CELL();
-    struct procedure* p = malloc(sizeof(struct procedure));
-
-    p->type = PRIMATIVE;
-    p->data.primative.f = f;
-    
-    scheme_set_carx(s, (PROCEDURE_T << 3) | 6);
-    scheme_set_cdrx(s, p);
-
-    return s;
-}
-
 scheme_t make_procedure(env_frame_t* env, scheme_t formals, scheme_t body)
 {
     scheme_t s = MAKE_CELL();
@@ -34,4 +20,27 @@ scheme_t make_procedure(env_frame_t* env, scheme_t formals, scheme_t body)
     scheme_set_cdrx(s, p);
 
     return s;
+}
+
+scheme_t make_primative(primative_t f, uint8_t req,
+			uint8_t opt, uint8_t rest)
+{
+    scheme_t s = MAKE_CELL();
+    struct procedure* p = malloc(sizeof(struct procedure));
+
+    p->type = PRIMATIVE;
+    p->data.primative.f = f;
+    p->data.nargs_req = req;
+    p->data.nargs_opt = opt;
+    p->data.rest = rest;
+    
+    scheme_set_carx(s, (PROCEDURE_T << 3) | 6);
+    scheme_set_cdrx(s, p);
+
+    return s;
+}
+
+scheme_t apply_primative(scheme_t rator, scheme_t rands)
+{
+    
 }
--- procedures.h
+++ procedures.h
@@ -9,11 +9,16 @@
 #include "scheme.h"
 #include "env.h"
 
+typedef scheme_t (*primative_t)();
+
 struct procedure {
     enum {PRIMATIVE, COMPOUND} type;
     union {
 	struct {
-	    scheme_t (*f)(scheme_t);
+	    primative_t f;
+	    uint8_t     nargs_req;
+	    uint8_t     nargs_opt;
+	    uint8_t     rest;
 	} primative;
 	struct {
 	    env_frame_t* env;        /* Reference to lexical environment */
@@ -27,6 +32,25 @@ struct procedure {
 #define MAKE_PROCEDURE(e, f, b) (make_procedure(e, a, b))
 #define GET_PROCEDURE(s)   ((struct procedure*)GET_CDR(GET_PTR(s)))
 
+/*
+ * Make a new compound procedure in the given lexical environment with
+ * the passed list of formals and list of body expressions.
+ */
 extern scheme_t make_procedure(env_frame_t*, scheme_t, scheme_t);
+
+/*
+ * Make a new primative procedure given a function pointer, the number
+ * of required arguments, optional arguments, and whether or not to
+ * pass a 'rest' argument.
+ */
+extern scheme_t make_primative(primative_t proc,
+			       uint8_t nargs_req,
+			       uint8_t nargs_opt,
+			       uint8_t rest);
+
+/*
+ * Apply a primative procedure to the list of arguments.
+ */
+extern scheme_t apply_primative(scheme_t rator, scheme_t rands);
 
 #endif
