4088a9a2c808143a12af1c312a0951085e3cc9b6
Dino A. Dai Zovi <3083278+ddz@users.noreply.github.com>
2002-05-01 00:40:55 +0000

    Updated det.scm.

--- bench/det.scm
+++ bench/det.scm
@@ -1,9 +1,3 @@
-(define map2
-  (lambda (f ls1 ls2)
-    (if (null? ls1)
-        '()
-        (cons (f (car ls1) (car ls2)) (map2 f (cdr ls1) (cdr ls2))))))
-
 (define add1
   (lambda (x)
     (+ x 1)))
@@ -21,19 +15,19 @@
 		  ()
 		  (cons (* j (* k (car ls)))
 			(alt-row j (* -1 k) (cdr ls))))))
-      (set! alt-col
-	    (lambda (l m ls)
-	      (if (null? ls)
-		  ()
-		  (cons (alt-row l m (car ls))
-			(alt-col (* -1 l) m (cdr ls))))))
-      (if (pair? (car ls))
-	  (alt-col 1 1 ls)
-	  (alt-row 1 1 ls)))))
+       (set! alt-col
+	     (lambda (l m ls)
+	       (if (null? ls)
+		   ()
+		   (cons (alt-row l m (car ls))
+			 (alt-col (* -1 l) m (cdr ls))))))
+       (if (pair? (car ls))
+	   (alt-col 1 1 ls)
+	   (alt-row 1 1 ls)))))
 
 (define sublist
   (lambda (ls n m)
-    (let ((tail 1) (head 1))
+    (let ((tail 1) (head 1)) 
       (set! tail
 	    (lambda (ls n)
 	      (if (= n 0)
@@ -47,13 +41,13 @@
        (head (tail ls n) (- m n)))))
 
 (define kill-row
-  (lambda (a r)
-    (append (sublist a 0 r)
-	    (sublist a (add1 r) (length a)))))
+  (lambda (A r)
+    (append (sublist A 0 r)
+            (sublist A (add1 r) (length A)))))
 
 (define kill-col
-  (lambda (a c)
-    (map (lambda (x) (kill-row x c)) a)))
+  (lambda (A c)
+    (map (lambda (x) (kill-row x c)) A)))
 
 (define iota
   (lambda (n)
@@ -66,23 +60,19 @@
       (loop n ()))))
 
 (define determinant
-  (lambda (a)
-    (let ((n (length a)))
+  (lambda (A)
+    (let ((n (length A)))
       (if (= n 1)
-	  (car (car a))
-	  (let ((B (kill-row a 0)))
+          (car (car A))
+          (let ((B (kill-row A 0)))
 	    (let ((minors (map (lambda (x) (kill-col B (sub1 x))) (iota n))))
-		 (let ((cofactors (map determinant minors)))
-		   (apply + (map2 * (alt (list-ref a 0)) cofactors)))))))))
+              (let ((cofactors (map determinant minors)))
+		(apply + (map2 * (alt (list-ref A 0)) cofactors)))))))))
 
-(define a '((1  2  3  4  5 4) 
-            (6 -5  4 -3  2  4) 
-            (1  3  6  2  4 4) 
-            (3  4  5 -1 -3 4) 
-	    (1  0 -1  0 -1 4)
-            (1 2 3 4 5 6)))
+(define A '((1  2  3  4  5) 
+            (6 -5  4 -3  2) 
+            (1  3  6  2  4) 
+            (3  4  5 -1 -3) 
+            (1  0 -1  0 -1)))
 
-(define b '((1 2) (3 4)))
-
-(determinant a)
-(determinant b)
+(determinant A)
